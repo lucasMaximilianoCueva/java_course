857a152bc60aa56bc943732fdd4802cfa9c83be3
Lucas Cueva <lucasmaximiliano@outlook.com>
2022-06-08 15:44:23 -0300

    1st challenge

---
+++ NumeroFlotanteCueva.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float num = 77.4301f;
+        int integer = (int)num;
+        float decimal = num - integer;
+        System.out.println("integer: " + integer + " decimal: " + decimal);
+    }
+}
